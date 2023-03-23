7015b8ca959dd0420f086dbe852a00fc59d551ab
Harsh <harsh1627.be22@chitkara.edu.in>
2023-03-23 22:52:36 +0530

    first

--- project.c
+++ project.c
@@ -36,11 +36,6 @@ printf("\n thanks for vote !!");
 
 void votesCount(){
 printf("\n\n ##### Voting Statics ####");
-printf("\n %s - %d ", CANDIDATE1, votesCount1);
-printf("\n %s - %d ", CANDIDATE1, votesCount2);
-printf("\n %s - %d ", CANDIDATE1, votesCount3);
-printf("\n %s - %d ", CANDIDATE1, votesCount4);
-printf("\n %s - %d ", "Spoiled Votes", spoiledtvotes); 
 }
 
 void getLeadingCandidate(){
